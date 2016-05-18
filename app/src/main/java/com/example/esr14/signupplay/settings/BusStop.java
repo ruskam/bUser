d13b820ad448eb4d763767d098318ad036f771b0
ESR14 <rustam.kamberov@gmail.com>
2016-05-18 20:04:39 +0100

    - adding BusStop bean to map the MongoDb response JSON

---
+++ app/src/main/java/com/example/esr14/signupplay/settings/BusStop.java
@@ -0,0 +1,10 @@
+package com.example.esr14.signupplay.settings;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+/**
+ * Created by ESR14 on 01/05/16.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BusStop {
+}
