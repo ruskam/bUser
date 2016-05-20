c7c2c176f47928b3f4c361f42b3572d88588f36d
ESR14 <rustam.kamberov@gmail.com>
2016-05-20 16:37:28 +0100

    - added Location class to map location data retreived from JSON web service response - revised BusStop bean to map the JSON bus stop information

--- app/src/main/java/com/example/esr14/signupplay/Model/BusStop.java
+++ app/src/main/java/com/example/esr14/signupplay/Model/BusStop.java
@@ -1,10 +1,62 @@
 package com.example.esr14.signupplay.Model;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
 
 
 /**
- * Created by ESR14 on 01/05/16.
+ * Created by Rustam Kamberov on 01/05/16.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BusStop {
+
+    private String id;
+    private  String stopName;
+    private Location location;
+    List<String> lines;
+    List<String> schedule;
+
+    public BusStop(){
+
+    }
+
+    public BusStop(String stopName, Location location, List<String> lines, List<String> schedule) {
+        this.stopName = stopName;
+        this.location = location;
+        this.lines = lines;
+        this.schedule = schedule;
+    }
+
+    public String getStopName() {
+        return stopName;
+    }
+
+    public void setStopName(String stopName) {
+        this.stopName = stopName;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<String> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<String> lines) {
+        this.lines = lines;
+    }
+
+    public List<String> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(List<String> schedule) {
+        this.schedule = schedule;
+    }
 }
---
+++ app/src/main/java/com/example/esr14/signupplay/Model/Location.java
@@ -0,0 +1,15 @@
+package com.example.esr14.signupplay.Model;
+
+/**
+ * Created by Rustam Kamberov on 20/05/16.
+ */
+public class Location {
+
+    private double x;
+    private double y;
+    private String type;
+    private String [] coordinates;
+
+
+
+}
