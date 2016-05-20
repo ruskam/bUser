5088a89b54ef7ff79968e84dfbbac8ae462289ab
ESR14 <rustam.kamberov@gmail.com>
2016-05-20 16:40:38 +0100

    - bug fixes

--- app/src/main/java/com/example/esr14/signupplay/Model/Location.java
+++ app/src/main/java/com/example/esr14/signupplay/Model/Location.java
@@ -1,5 +1,7 @@
 package com.example.esr14.signupplay.Model;
 
+import java.util.Arrays;
+
 /**
  * Created by Rustam Kamberov on 20/05/16.
  */
@@ -8,8 +10,60 @@ public class Location {
     private double x;
     private double y;
     private String type;
-    private String [] coordinates;
+    private String[] coordinates;
 
+    public Location() {
+    }
+
+    public Location(double x, double y, String type, String[] coordinates) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.coordinates = coordinates;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "x=" + x +
+                ", y=" + y +
+                ", type='" + type + '\'' +
+                ", coordinates=" + Arrays.toString(coordinates) +
+                '}';
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String[] coordinates) {
+        this.coordinates = coordinates;
+    }
 
 
 }
