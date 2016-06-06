f66b0c43d9e552c55f57cfd743b06aba5fb0435d
ESR14 <rustam.kamberov@gmail.com>
2016-06-06 16:50:19 +0100

    - minor bug fixes

--- app/src/main/java/com/example/esr14/signupplay/BusStopAct.java
+++ app/src/main/java/com/example/esr14/signupplay/BusStopAct.java
@@ -3,14 +3,13 @@ package com.example.esr14.signupplay;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.esr14.signupplay.Model.BusStop;
 import com.example.esr14.signupplay.settings.SettingsActivity;
-import com.example.esr14.signupplay.util.AbstractActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,10 +26,7 @@ import java.util.Map;
 /**
  * This class is called after a user is logged in
  */
-
-public class BusStopAct extends AbstractActivity {
-    final String DEV = "device";
-    Button retry;
+public class BusStopAct extends AppCompatActivity {
 
     TextView tvNextBusTime;
 
@@ -38,38 +34,16 @@ public class BusStopAct extends AbstractActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_stop);
-        retry = (Button) findViewById(R.id.button_retry);
+
         tvNextBusTime = (TextView) findViewById(R.id.tvNextBusTime);
 
-        connect();
-        if (isBtConnected) {
-            msg("ok");
-            retry.setText("Connected");
-        }
-        else {
-            msg("Failed to connect");
-        }
-        retry.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                    msg("Retrying to connect");
-                    if(connect())
-                         retry.setText("Connected");
-
-            }
-        });
     }
-@Override
-public void detectedCard() {
-    startActivity(new Intent(this,insideBus.class));
-}
+
     public void displaySettings(View view) {
         startActivity(new Intent(this, SettingsActivity.class));
     }
 
-    public void getInsideBus(View view) {
+    public void getInsideBus(View view){
         startActivity(new Intent(this, insideBus.class));
     }
 
@@ -104,8 +78,7 @@ public void detectedCard() {
 
         @Override
         protected void onPostExecute(BusStop busStop) {
-
-            try {
+            try{
                 Log.i("info", busStop.getStopName());
 
                 List<String> schedule = busStop.getSchedule();
@@ -189,7 +162,4 @@ public void detectedCard() {
         }
 
     }
-
-
-
 }
